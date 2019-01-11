856334fd3803cad28dd44230a6bc74efd15f524d
Ryan Gittings <ryangittings@Kirsties-MacBook-Pro.local>
2019-01-11 16:18:06 +0000

    INITIAL COMMIT

---
+++ PerchRunwayValetDriver.php
@@ -0,0 +1,38 @@
+<?php
+
+class PerchRunwayValetDriver extends BasicValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        if (strpos($uri, 'admin') == false || strpos($uri, 'perch') == false) {
+          return is_dir($sitePath.'/admin/core/runway');
+        }
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        $_SERVER['PHP_SELF']    = $uri;
+        $_SERVER['SERVER_ADDR'] = '127.0.0.1';
+        $_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'];
+
+        return parent::frontControllerPath(
+          $sitePath, $siteName, '/admin/core/runway/start.php'
+        );
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Perch Runway Valet Driver
+
+This is a Perch Runway driver for Laravel Valet.
+
+## Installation
+
+Add the `PerchRunwayValetDriver.php` file to `/Users/{your_username}/.valet/Drivers`. 
+
+## More Info
+
+More information on how Laravel Valet custom drivers work can be found: [https://laravel.com/docs/5.4/valet#custom-valet-drivers](https://laravel.com/docs/5.4/valet#custom-valet-drivers)
